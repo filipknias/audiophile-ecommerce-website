f697db1cfa9782153bc58e9921ba987dcab353bd
Filip <filip.knias@o2.pl>
2022-12-25 18:01:16 +0100

    initial theme palette created

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,17 @@
 import { DefaultTheme } from 'styled-components';
 
 export const theme: DefaultTheme = {
-  
+  text: {
+    light: 'rgba(0,0,0,0.5)',
+    white: '#fff',
+    black: '#191919',
+  },
+  palette: {
+    orange: '#d87d4a',
+    gray: '#f1f1f1',
+  },
+  background: {
+    light: '#fafafa',
+    dark: '#191919',
+  }
 }
--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -2,6 +2,18 @@ import 'styled-components';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
-    
+    text: {
+      light: string;
+      white: string;
+      black: string;
+    },
+    palette: {
+      orange: string;
+      gray: string;
+    },
+    background: {
+      light: string;
+      dark: string;
+    }
   }
 }
