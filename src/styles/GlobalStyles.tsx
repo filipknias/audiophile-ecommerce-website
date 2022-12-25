11de161bfec96dd3f315a198ca375bd8b6025f4f
Filip <filip.knias@o2.pl>
2022-12-25 17:38:11 +0100

    add lato font family in global styles component

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,9 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap');
+
   * {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+    font-family: 'Lato', sans-serif;
   }
 `;
