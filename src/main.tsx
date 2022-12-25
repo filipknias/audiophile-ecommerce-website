1577e3d8599cbabab292728b5337a0ce71b3e27d
Filip <filip.knias@o2.pl>
2022-12-25 17:16:54 +0100

    install styled-components and setup theme provider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { ThemeProvider } from 'styled-components';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={{}}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
