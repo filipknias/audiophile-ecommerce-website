092be0c8ea25be1c8ab626fac587bd17f813d93d
Filip <filip.knias@o2.pl>
2022-12-26 10:04:39 +0100

    create products helper file

---
+++ src/utilities/products.ts
@@ -0,0 +1,13 @@
+import productsData from 'data/products.json';
+
+export const getCategories = () => {
+  return new Set(productsData.products.map((product) => product.category));
+}
+
+export const getProductById = (id: number) => {
+  return productsData.products.filter((product) => product.id === id);
+}
+
+export const getProductBySlug = (slug: string) => {
+  return productsData.products.filter((product) => product.slug === slug);
+}
