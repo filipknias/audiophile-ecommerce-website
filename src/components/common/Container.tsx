95b263f04102e41698cad6672b5614a4de306eba
Filip <filip.knias@o2.pl>
2022-12-27 20:51:19 +0100

    change container width

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -1,12 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 1400px;
+  width: 1200px;
   margin: 0 auto;
   padding: 0px 15px;
-  @media screen and (max-width: ${({ theme }) => theme.breakpoints.lg}) {
-    width: 100%;
-  }
+
   @media screen and (max-width: ${({ theme }) => theme.breakpoints.md}) {
     width: 100%;
   }
