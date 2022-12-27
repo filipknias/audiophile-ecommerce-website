b72cb44f756b2a5cc7894ef42b8bb7968e915f37
Filip <filip.knias@o2.pl>
2022-12-27 20:19:51 +0100

    remove unused import

--- src/components/products/ZX7Speaker/ZX7Speaker.tsx
+++ src/components/products/ZX7Speaker/ZX7Speaker.tsx
@@ -1,6 +1,5 @@
 import { Wrapper, Content, Title } from './styles';
 import { OutlinedButton } from 'components/common';
-import SpeakerImage from 'assets/images/home/desktop/image-speaker-zx7.jpg';
 
 export const ZX7Speaker = (): JSX.Element => {
   return (
