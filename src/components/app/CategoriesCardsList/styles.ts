c987bb503bdc559a647315c61515e6a5ac1632db
Filip <filip.knias@o2.pl>
2022-12-28 20:17:27 +0100

    add top margin top categories cards list component

--- src/components/app/CategoriesCardsList/styles.ts
+++ src/components/app/CategoriesCardsList/styles.ts
@@ -4,6 +4,7 @@ export const CategoriesWrapper = styled.div`
   width: 100%;
   display: flex;
   gap: 40px;
+  margin-top: 2rem;
   @media screen and (max-width: ${({ theme }) => theme.breakpoints.sm}) {
     gap: 120px;
     flex-direction: column;
