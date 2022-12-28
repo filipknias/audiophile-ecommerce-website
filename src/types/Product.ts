5a481dd76b90c745be77ac82ad5006276edabbf8
Filip <filip.knias@o2.pl>
2022-12-28 13:22:53 +0100

    add product model types

---
+++ src/types/Product.ts
@@ -0,0 +1,34 @@
+interface Breakpoints {
+  mobile: string;
+  tablet: string;
+  desktop: string;
+} 
+
+export interface Product {
+    id: number;
+    slug: string;
+    name: string;
+    shortName: string;
+    image: Breakpoints;
+    cartImage: string;
+    category: string;
+    categoryImage: Breakpoints;
+    new: boolean;
+    price: number;
+    description: string;
+    features: string;
+    includedItems: {
+        quantity: number;
+        item: string;
+    }[];
+    gallery: {
+        first: Breakpoints;
+        second: Breakpoints;
+        third: Breakpoints;
+    }
+    others: {
+        slug: string;
+        name: string;
+        image: Breakpoints;
+    }[];
+}
