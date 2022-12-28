def3966844f25364e18f8afc02464db5633a6550
Filip <filip.knias@o2.pl>
2022-12-28 12:27:59 +0100

    change get categories function to return array instead of set

--- src/utilities/products.ts
+++ src/utilities/products.ts
@@ -1,7 +1,7 @@
 import productsData from 'data/products.json';
 
 export const getCategories = () => {
-  return new Set(productsData.products.map((product) => product.category));
+  return [...new Set(productsData.products.map((product) => product.category))];
 }
 
 export const getProductById = (id: number) => {
