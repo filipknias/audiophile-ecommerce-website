dec6798c61df2421709d50cf4f77b67baf126239
Filip <filip.knias@o2.pl>
2022-12-29 09:38:49 +0100

    edit products utils file to return only first found product

--- src/utilities/products.ts
+++ src/utilities/products.ts
@@ -5,11 +5,11 @@ export const getCategories = () => {
 }
 
 export const getProductById = (id: number) => {
-  return productsData.products.filter((product) => product.id === id);
+  return productsData.products.filter((product) => product.id === id)[0];
 }
 
 export const getProductBySlug = (slug: string) => {
-  return productsData.products.filter((product) => product.slug === slug);
+  return productsData.products.filter((product) => product.slug === slug)[0];
 }
 
 export const getProductByCategory = (category: string) => {
