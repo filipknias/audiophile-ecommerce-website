d930e64f3cec9766aa8a63fd2dacf9b88600fa2f
Filip <filip.knias@o2.pl>
2022-12-30 11:31:54 +0100

    setup redux toolkit store and cart reducer

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,18 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'styles/GlobalStyles';
 import { theme } from 'styles/theme';
 import { BrowserRouter } from "react-router-dom";
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 )
---
+++ src/redux/features/cartSlice.ts
@@ -0,0 +1,69 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+
+interface CartItem {
+  id: number;
+  slug: string;
+  image: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export interface CartState {
+  cart: CartItem[];
+  total: number;
+}
+
+const initialState: CartState = {
+  cart: [],
+  total: 0,
+}
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem: (state, { payload }: PayloadAction<CartItem>) => {
+      state.cart = [...state.cart, payload];
+      state.total += payload.price * payload.quantity;
+    },
+    removeItem: (state, { payload }: PayloadAction<number>) => {
+      const cartItem = state.cart.find((item) => item.id === payload);
+      if (!cartItem) return;
+      state.cart = state.cart.filter((item) => item.id !== cartItem.id);
+      state.total -= cartItem.price * cartItem.quantity;;
+    },
+    increaseItemQuantity: (state, { payload }: PayloadAction<number>) => {
+      const cartItem = state.cart.find((item) => item.id === payload);
+      if (!cartItem) return;
+      state.cart = state.cart.map((item) => {
+        if (item.id === payload) {
+          return {
+            ...item,
+            quantity: item.quantity + 1,
+          }
+        }
+        return item;
+      });
+      state.total += cartItem.price;
+    },
+    decreaseItemQuantity: (state, { payload }: PayloadAction<number>) => {
+      const cartItem = state.cart.find((item) => item.id === payload);
+      if (!cartItem) return;
+      state.cart = state.cart.map((item) => {
+        if (item.id === payload) {
+          return {
+            ...item,
+            quantity: item.quantity - 1,
+          }
+        }
+        return item;
+      });
+      state.total -= cartItem.price;
+    },
+  },
+})
+
+export const { addItem, removeItem, increaseItemQuantity, decreaseItemQuantity } = cartSlice.actions;
+export default cartSlice.reducer;
