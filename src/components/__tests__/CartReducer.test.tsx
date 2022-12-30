ba0d774db7e5a1b7b74e4b3bf3a791e743719582
Filip <filip.knias@o2.pl>
2022-12-30 13:10:29 +0100

    add cart reducer test cases

--- src/components/__tests__/CartReducer.test.tsx
+++ src/components/__tests__/CartReducer.test.tsx
@@ -1,7 +1,17 @@
 import { getProductById } from "utilities/products";
-import reducer, { addItem } from "redux/features/cartSlice";
+import reducer, { addItem, removeItem, increaseItemQuantity, decreaseItemQuantity } from "redux/features/cartSlice";
 
 describe("cart reducer", () => {
+  const product = getProductById(1);
+  const mockCartItem = {
+    id: product.id,
+    slug: product.slug,
+    quantity: 1,
+    image: product.cartImage,
+    name: product.name,
+    price: product.price,
+  };
+
   it("should add item to cart", () => {
     const product = getProductById(1);
 
@@ -10,18 +20,49 @@ describe("cart reducer", () => {
       total: 0,
     };
 
-    const cartItem = {
-      id: product.id,
-      slug: product.slug,
-      quantity: 1,
-      image: product.cartImage,
-      name: product.name,
-      price: product.price,
-    }
+    expect(reducer(prevState, addItem(mockCartItem))).toEqual({
+      cart: [mockCartItem],
+      total: product.price * mockCartItem.quantity,
+    });
+  });
 
-    expect(reducer(prevState, addItem(cartItem))).toEqual({
-      cart: [cartItem],
-      total: product.price * cartItem.quantity,
+  it("should remove item to cart", () => {
+    const prevState = {
+      cart: [mockCartItem],
+      total: mockCartItem.price * mockCartItem.quantity,
+    };
+
+    expect(reducer(prevState, removeItem(mockCartItem.id))).toEqual({
+      cart: [],
+      total: 0,
+    });
+  });
+
+  it("should increase item quantity", () => {
+    const prevState = {
+      cart: [mockCartItem],
+      total: mockCartItem.price * mockCartItem.quantity,
+    };
+
+    const newQuantity = mockCartItem.quantity + 1;
+
+    expect(reducer(prevState, increaseItemQuantity(mockCartItem.id))).toEqual({
+      cart: [{ ...mockCartItem, quantity: mockCartItem.quantity + 1 }],
+      total: mockCartItem.price * newQuantity,
+    });
+  });
+
+  it("should decrease item quantity", () => {
+    const prevState = {
+      cart: [mockCartItem],
+      total: mockCartItem.price * mockCartItem.quantity,
+    };
+
+    const newQuantity = mockCartItem.quantity - 1;
+
+    expect(reducer(prevState, decreaseItemQuantity(mockCartItem.id))).toEqual({
+      cart: [{ ...mockCartItem, quantity: mockCartItem.quantity - 1 }],
+      total: mockCartItem.price * newQuantity,
     });
   });
 });
