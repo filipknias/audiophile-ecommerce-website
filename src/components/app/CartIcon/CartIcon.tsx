ef938a5f44f277b5bc4162e20e9c8530d0bc4d96
Filip <filip.knias@o2.pl>
2022-12-30 11:37:38 +0100

    display redux cart length value in cart icon component

--- src/components/app/CartIcon/CartIcon.tsx
+++ src/components/app/CartIcon/CartIcon.tsx
@@ -1,11 +1,14 @@
 import CartImage from 'assets/images/shared/desktop/icon-cart.svg';
 import { Wrapper, Cart, AmountDisplay } from './styles';
+import { useAppSelector } from 'redux/hooks';
 
 export const CartIcon = (): JSX.Element => {
+  const { cart } = useAppSelector((state) => state.cart)
+
   return (
     <Wrapper>
       <Cart src={CartImage} alt="cart-icon" />
-      <AmountDisplay>1</AmountDisplay>
+      {cart.length > 0 && <AmountDisplay>{cart.length}</AmountDisplay>}
     </Wrapper>
   )
 }
