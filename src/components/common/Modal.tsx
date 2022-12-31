ddf1eb68ecc5659392881774433cec0373b542ba
Filip <filip.knias@o2.pl>
2022-12-31 11:53:56 +0100

    hide modal on click outside implemented

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,4 +1,8 @@
 import styled from "styled-components";
+import { useClickOutside } from "hooks/useClickOutside";
+import { useRef } from "react";
+import { useAppDispatch } from "redux/hooks";
+import { hideModal } from "redux/features/modalSlice";
 
 const Overlay = styled.div`
   position: fixed;
@@ -6,7 +10,7 @@ const Overlay = styled.div`
   left: 0;
   width: 100vw;
   height: 100vh;
-  background-color: rgba(0, 0, 0, 0.8);
+  background-color: ${({ theme }) => theme.background.modal};
   @media screen and (max-width: ${({ theme }) => theme.breakpoints.md}) {
     padding: 1rem;
   }
@@ -20,7 +24,8 @@ const ModalWrapper = styled.div`
   background-color: ${({ theme }) => theme.background.light};
   border-radius: ${({ theme }) => theme.borderRadius};
   padding: 2rem 1rem;
-  width: 500px;
+  min-width: 500px;
+  animation: ${({ theme }) => `${theme.animations.fade} ${theme.transitions.fast} ease-in`};
   @media screen and (max-width: ${({ theme }) => theme.breakpoints.sm}) {
     width: 90%;
     margin: 0 auto;
@@ -32,9 +37,16 @@ interface Props {
 }
 
 export const Modal = ({ children }: Props): JSX.Element => {
+  const modalRef = useRef<HTMLDivElement|null>(null);
+  const dispatch = useAppDispatch();
+
+  useClickOutside(modalRef, () => {
+    dispatch(hideModal());
+  });
+
   return (
     <Overlay>
-      <ModalWrapper>
+      <ModalWrapper ref={modalRef}>
         {children}
       </ModalWrapper>
     </Overlay>
