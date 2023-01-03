dfa8d61aba230fcaf0cf1d1f41fb04e840837581
Filip <filip.knias@o2.pl>
2023-01-03 09:35:12 +0100

    scroll to top on url change

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { Navbar, Footer } from "components/app";
 import { Home, Category, Product, Checkout } from "pages";
 import { ToastContainer } from 'react-toastify';
 import { useAppSelector } from "redux/hooks";
+import { useEffect } from "react";
 
 export default (): JSX.Element => {
   const { activeModal } = useAppSelector((state) => state.modal);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <>
       <Navbar />
